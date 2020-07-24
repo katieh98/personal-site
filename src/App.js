e75964dc05d023fc64047e88313ebe49d041a951
Kate Harold <katieh98@vt.edu>
2020-07-24 14:27:11 -0400

    Update App.js
    Added resize hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter,
   Switch,
@@ -13,8 +14,10 @@ import Home from "./components/Home"
 import Footer from "./components/Footer"
 
 export default function App() {
+  const size = useWindowSize();
   return (
     <div className="App">
+    {size.width}px / {size.height}px
     <Header />
     <BrowserRouter>
       <NavBar />
@@ -26,4 +29,33 @@ export default function App() {
     <Footer />
     </div>
   );
+}
+
+// Hook to resize
+function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
 }
