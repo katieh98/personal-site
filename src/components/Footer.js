a92d54577ff3c6b9f1971f9837badbf7158aafae
Kate Harold <katieh98@vt.edu>
2020-07-24 15:43:21 -0400

    spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,19 @@ import React from 'react'
 class Footer extends React.Component {
   render() {
     return (<footer className="footer">
+              <div className="text-center">
+                <a href="mailto:kate.harold@pm.me">
+                  <img src={email} width="30" height="30" hspace="5" vspace="5" alt="email" />
+                </a>
+
+                <a href="https://www.linkedin.com/in/kate-harold/" target="_blank">
+                  <img src={link} width="30" height="30" hspace="5" vspace="5" alt="link" />
+                </a>
+
+                <a href="https://github.com/katieh98" target="_blank">
+                  <img src={git} width="30" height="30" hspace="5" vspace="5" alt="git" />
+                </a>
+                </div>
     Copyright © 2020 Kate Harold || Built by Kate Harold
         </footer>);
   }
