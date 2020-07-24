8bd4f81dbd7173a08067f39d7c43a71cb278a503
Kate Harold <katieh98@vt.edu>
2020-07-24 16:08:41 -0400

    Update Socials.js
    git is going in the middle now

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -11,12 +11,12 @@ class Socials extends React.Component {
           <img src={email} width="30" height="30" hspace="5" vspace="5" alt="email" />
         </a>
 
-        <a href="https://www.linkedin.com/in/kate-harold/" target="_blank">
-          <img src={link} width="30" height="30" hspace="5" vspace="5" alt="link" />
-        </a>
-
         <a href="https://github.com/katieh98" target="_blank">
           <img src={git} width="30" height="30" hspace="5" alt="git" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/kate-harold/" target="_blank">
+          <img src={link} width="30" height="30" hspace="5" vspace="5" alt="link" />
         </a>
         </div>
     );
