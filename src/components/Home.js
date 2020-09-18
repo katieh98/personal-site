549fe94fc3a6e1fad5674efd74e28f8627fb2fd6
Kate Harold <katieh98@vt.edu>
2020-09-18 16:38:30 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,18 @@ import cal from "./images/calendar.png"
 class Home extends React.Component {
   render() {
     return (<div className="container">
+    <h3>Hello Visitor!</h3>
+    <p>
+      I'm an undergraduate student at Virginia Tech pursuing my degree in Computer Science.
+      Currently, I live in Northern Virginia and am searching for full time employment after
+      I graduate in December 2020. I have experience with software development through
+       my internships and personal projects but am always looking to learn more. My
+        interests include watching movies, cooking, traveling (even though I haven't gone many
+       places), and playing games with friends.
+       <br /><br />
+       My inbox is always open! Feel free to contact me whether it's to just say hi or ask questions.
+       <br />Please feel free to make suggestions for the site since this is my first time using ReactJS!
+    </p>
       <br /> <br />
       <h4>Education</h4>
         <p><strong>B.S. Computer Science</strong><br/>
