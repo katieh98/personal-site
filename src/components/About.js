32ba7a77c38f3d29e93606deacba705a55d28209
Kate Harold <katieh98@vt.edu>
2020-09-18 16:16:32 -0400

    Update About.js
    Took out picture b/c upside down on iOS

--- src/components/About.js
+++ src/components/About.js
@@ -19,17 +19,17 @@ class About extends React.Component {
                My inbox is always open! Feel free to contact me whether it's to just say hi or ask questions.
             </p>
 
-            <Figure>
-              <Figure.Image
-                width={300}
-                height={300}
-                alt="Travel pic"
-                src={travel}
-                />
-            <Figure.Caption>
-              My last travel destination: Grand Cayman Islands
-              </Figure.Caption>
-              </Figure>
+            // <Figure>
+            //   <Figure.Image
+            //     width={300}
+            //     height={300}
+            //     alt="Travel pic"
+            //     src={travel}
+            //     />
+            // <Figure.Caption>
+            //   My last travel destination: Grand Cayman Islands
+            //   </Figure.Caption>
+            //   </Figure>
           </div>);
   }
 }
