fe7cbcf19f6c8d1ac9313b11d78474b7c0860cad
katieh98 <katieh98@vt.edu>
2021-03-09 14:17:12 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,11 @@ import { WindupChildren, Pause, Pace } from "windups";
 class About extends React.Component {
   render() {
     return (<div className="container"><br/><br/>
-            <h3><StringyWindup/></h3>]
-            <h3><Titles/></h3>
+            <h3><StringyWindup/></h3>
             <p>
-              I'm a Software Engineer in the Northern Virginia/DC area. I completed my undergraduate degree in Computer Science at <a href="https://vt.edu/">Virginia Tech</a>.
-               <br /><br />
-               My inbox is always open! Feel free to contact me whether it's to just say hi or ask questions.
+            I'm a Software Engineer in the Northern Virginia/Washington DC area.
+            I completed my undergraduate degree in Computer Science at <a href="https://vt.edu/">Virginia Tech</a>.
+             <br /><br />
             </p>
 
             <Figure>
@@ -24,45 +23,19 @@ class About extends React.Component {
                 />
             <Figure.Caption>
               My last travel destination: Grand Cayman Islands
-              </Figure.Caption>
-              </Figure>
+  </Figure.Caption>
+</Figure>
           </div>);
   }
 }
 
-const var list = {'foodie!', 'movie lover!', 'adoptee!', 'anime watcher!', 'runner!', 'Software Engineer!'}
-
 const StringyWindup = () => {
   return (
     <WindupChildren >
-      {"Hello Visitor! \n"}
+      {"Hello Visitor!"}
       < Pause ms={100} />
-      <Pace ms={50}>
-        {"I am a..."}
-      </Pace>
-
     </WindupChildren>
   );
 };
-
-const Titles = () => {
-  return (
-    <WindupChildren>
-      {'foodie!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'movie lover!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'anime lover!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'runner!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'Software Engineer!'}
-    </WindupChildren>
-  )
-}
 
 export default About
