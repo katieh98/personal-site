d793c8282a3375231a2644ca94ca836ed75228b0
katieh98 <katieh98@vt.edu>
2021-03-09 14:59:31 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -28,25 +28,7 @@ class About extends React.Component {
                <br /><br />
                My inbox is always open! Feel free to contact me whether it's to just say hi or ask questions.
             </p>
-<<<<<<< HEAD
-          </div>
-          <Gallery photos={photos} />
-        );
-=======
-
-            <Figure>
-              <Figure.Image
-                width={300}
-                height={300}
-                alt="Travel pic"
-                src={travel}
-                />
-            <Figure.Caption>
-              My last travel destination: Grand Cayman Islands
-              </Figure.Caption>
-              </Figure>
           </div>);
->>>>>>> parent of fe7cbcf1 (Update About.js)
   }
 }
 
@@ -55,41 +37,13 @@ const var list = {'foodie!', 'movie lover!', 'adoptee!', 'anime watcher!', 'runn
 const StringyWindup = () => {
   return (
     <WindupChildren >
-<<<<<<< HEAD
     <Pace ms = {50}>
       {"Hello Visitor!"}
       < Pause ms={100} />
       </Pace>
-=======
-      {"Hello Visitor! \n"}
-      < Pause ms={100} />
-      <Pace ms={50}>
-        {"I am a..."}
-      </Pace>
-
->>>>>>> parent of fe7cbcf1 (Update About.js)
     </WindupChildren>
-  );
 };
 
-const Titles = () => {
-  return (
-    <WindupChildren>
-      {'foodie!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'movie lover!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'anime lover!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'runner!'}
-      < Pause ms = {100} />
-      useRewind();
-      {'Software Engineer!'}
-    </WindupChildren>
-  )
 }
 
 export default About
