b56857488af34bfaa39b0ac033fcbbe91d0ad1a9
katieh98 <katieh98@vt.edu>
2021-03-09 14:55:06 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -24,8 +24,6 @@ class About extends React.Component {
             <h3><StringyWindup/></h3>]
             <h3><Titles/></h3>
             <p>
-            I'm a Software Engineer in the Northern Virginia/Washington DC area.
-            I completed my undergraduate degree in Computer Science at <a href="https://vt.edu/">Virginia Tech</a>.
               I'm a Software Engineer in the Northern Virginia/DC area. I completed my undergraduate degree in Computer Science at <a href="https://vt.edu/">Virginia Tech</a>.
                <br /><br />
                My inbox is always open! Feel free to contact me whether it's to just say hi or ask questions.
