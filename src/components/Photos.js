644ab09326b340991f282808230e957b34892db6
Kate <katieh98@vt.edu>
2021-03-18 21:02:00 -0400

    forgot the photos

---
+++ src/components/Photos.js
@@ -0,0 +1,37 @@
+export const photos = [
+    {
+      src: "https://i.ibb.co/bXWCMZ4/49686238-1351250891682700-5111495026270011392-n.jpg",
+      width: 3,
+      height: 2
+    },
+  
+    {
+      src: "https://i.ibb.co/RHJpSSy/pumpkin.jpg",
+      width: 2,
+      height: 3
+    },
+  
+    {
+      src: "https://i.ibb.co/HXBXfPr/IMG-4010.jpg",
+      width: 2,
+      height: 3
+    },
+  
+    {
+      src :"https://i.ibb.co/brQXmRS/lion.jpg",
+      width: 3,
+      height: 2
+    },
+  
+    {
+      src: "https://i.ibb.co/fC15MvX/hiking.jpg",
+      width: 4,
+      height: 3
+    },
+  
+    {
+      src: "https://i.ibb.co/HHYxbG1/cascades.jpg",
+      width: 3,
+      height: 4
+    }
+  ];
